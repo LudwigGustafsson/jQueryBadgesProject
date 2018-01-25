972a2361e0188ef84f88488f3bc1d5aaea7a4367
Ludwig <ludwig.f.gustafsson@gmail.com>
2018-01-25 11:24:37 +0100

    second

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,11 +1,37 @@
 $(function() {
-
   $.ajax({
     url: 'https://www.codeschool.com/users/4547835.json',
     dataType: 'jsonp',
     success: function(response) {
+		addCourses(response.courses.completed);
       console.log('response', response);
-      // handle response
     }
   });
+
+  function addCourses(courses)
+  {
+	  var $badges = $('#badges');
+	  courses.forEach(function(course)
+	  {
+		  var $course = $('<div />',
+		  {
+			  'class': 'course'
+		  }).appendTo($badges);
+		  $('<h3 />',
+		  {
+			  text: course.title
+		  }).appendTo($course);
+		  $('<img />',
+		  {
+			  src: course.badge
+		  }).appendTo($course);
+		  $('<a />',
+		  {
+			  'class': 'btn btn-primary',
+			  target: '_blank',
+			  href: course.url,
+			  text: 'See Course'
+		  }).appendTo($course);
+	})
+  }
 });
